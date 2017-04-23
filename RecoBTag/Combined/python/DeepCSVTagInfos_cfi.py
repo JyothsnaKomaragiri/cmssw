f5d67a1cf302303cdd7348e370eb5209f29e323c
Jyothsna <jyothsna@cern.ch>
2017-04-23 19:34:26 +0200

    Updated producer to TrackDeepNNTagInfoProducer in cfi

--- RecoBTag/Combined/python/DeepCSVTagInfos_cfi.py
+++ RecoBTag/Combined/python/DeepCSVTagInfos_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 from RecoBTag.SecondaryVertex.combinedSecondaryVertexCommon_cff import combinedSecondaryVertexCommon
 
 pfDeepCSVTagInfos = cms.EDProducer(
-	'DeepNNTagInfoProducer',
+	'TrackDeepNNTagInfoProducer',
         trackIPTagInfos = cms.InputTag('ImpactParameterTagInfos'),
 	svTagInfos = cms.InputTag('InclusiveSecondaryVertexFinderTagInfos'),
 	computer = combinedSecondaryVertexCommon
